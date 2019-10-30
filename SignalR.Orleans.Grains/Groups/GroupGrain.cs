d4cce77a0e6330d596703b9dac31284c7c19466a
Clayton Lautier <clayton.lautier@outlook.com>
2019-10-30 16:32:03 +0100

    rework

---
+++ SignalR.Orleans.Grains/Groups/GroupGrain.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace SignalR.Orleans.Grains.Groups
+//{
+//    [StorageProvider(ProviderName = Constants.STORAGE_PROVIDER)]
+//    internal class GroupGrain : ConnectionGrain<GroupState>, IGroupGrain
+//    {
+//        public GroupGrain(ILogger<GroupGrain> logger) : base(logger)
+//        {
+//        }
+//    }
+
+//    internal class GroupState : ConnectionState
+//    {
+//    }
+//}
---
+++ SignalR.Orleans.Grains/Users/UserGrain.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using SignalR.Orleans.Grains.Core;
+
+//namespace SignalR.Orleans.Grains.Users
+//{
+//    [StorageProvider(ProviderName = Constants.STORAGE_PROVIDER)]
+//    internal class UserGrain : ConnectionGrain<UserState>, IUserGrain
+//    {
+//        public UserGrain(ILogger<UserGrain> logger) : base(logger)
+//        {
+//        }
+//    }
+
+//    internal class UserState : ConnectionState
+//    {
+//    }
+//}
